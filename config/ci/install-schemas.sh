5ea8cfcd8c74fbda23771e9afdd231b93ea54dcf
Eric Charpentier <eric.charpentier@ca.ibm.com>
2024-07-17 17:09:31 -0400

    First pass

--- config/ci/install-schemas.sh
+++ config/ci/install-schemas.sh
@@ -2,9 +2,9 @@
 
 echo "Install Apache Maven"
 
-curl -L https://archive.apache.org/dist/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz |
+curl -L https://archive.apache.org/dist/maven/maven-3/3.9.8/binaries/apache-maven-3.9.8-bin.tar.gz |
   tar -xzf - -C /usr/local &&
-  ln -s /usr/local/apache-maven-3.6.2 /usr/local/apache-maven &&
+  ln -s /usr/local/apache-maven-3.9.8 /usr/local/apache-maven &&
   ln -s /usr/local/apache-maven/bin/mvn /usr/local/bin/
 
 echo "Install the Divolte schema"
